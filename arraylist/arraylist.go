2ad8ce8396161825540180db3247662885b2521a
Hidetatz Yaginuma <hidetatz@gmail.com>
2021-12-03 10:53:58 +0900

    fix interface and add some method

--- arraylist/arraylist.go
+++ arraylist/arraylist.go
@@ -8,7 +8,15 @@ func New[T any]() *ArrayList[T] {
 	return &ArrayList[T]{}
 }
 
-func(a *ArrayList[T]) Add(e T) {
+func(a *ArrayList[T]) Append(e T) {
     a.values = append(a.values, e)
 }
 
+func(a *ArrayList[T]) AppendAll(es []T) {
+    a.values = append(a.values, es...)
+}
+
+func(a *ArrayList[T]) Insert(i int, e T) {
+    a.values = append(a.values[:i+1], a.values[i:]...)
+    a.values[i] = e
+}
--- arraylist/arraylist_test.go
+++ arraylist/arraylist_test.go
@@ -5,11 +5,21 @@ import (
 	"testing"
 )
 
-func TestAdd(t *testing.T) {
+func TestAppend(t *testing.T) {
 	al := New[int]()
-	al.Add(1)
-	al.Add(2)
+	al.Append(1)
+	al.Append(2)
 	if !reflect.DeepEqual(al.values, []int{1, 2}) {
 		t.Errorf("expected: %v but got %v", []int{1, 2}, al.values)
+	}
+}
+
+func TestAppendAll(t *testing.T) {
+	al := New[int]()
+	al.Append(1)
+	al.AppendAll([]int{2, 3, 4})
+	al.AppendAll([]int{5})
+	if !reflect.DeepEqual(al.values, []int{1, 2, 3, 4, 5}) {
+		t.Errorf("expected: %v but got %v", []int{1, 2, 3, 4, 5}, al.values)
 	}
 }
