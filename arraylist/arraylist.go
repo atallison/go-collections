27d8db305957f0c9069f5c6ed70c2893230c2d4a
Hidetatz Yaginuma <hidetatz@gmail.com>
2021-12-03 19:14:27 +0900

    add a method

--- arraylist/arraylist.go
+++ arraylist/arraylist.go
@@ -1,22 +1,49 @@
 package arraylist
 
+import (
+	"errors"
+)
+
+var (
+	InvalidIndexErr = errors.New("invalid index")
+)
+
 type ArrayList[T any] struct {
-    values []T
+	values []T
 }
 
 func New[T any]() *ArrayList[T] {
 	return &ArrayList[T]{}
 }
 
-func(a *ArrayList[T]) Append(e T) {
-    a.values = append(a.values, e)
+func(a *ArrayList[T]) Add(e T) {
+	a.values = append(a.values, e)
 }
 
-func(a *ArrayList[T]) AppendAll(es []T) {
-    a.values = append(a.values, es...)
+func(a *ArrayList[T]) AddAll(es []T) {
+	a.values = append(a.values, es...)
 }
 
-func(a *ArrayList[T]) Insert(i int, e T) {
-    a.values = append(a.values[:i+1], a.values[i:]...)
-    a.values[i] = e
+func(a *ArrayList[T]) AddAt(i int, e T) error {
+	if i < 0 || len(a.values) < i {
+		return InvalidIndexErr
+	}
+
+	if len(a.values) == i {
+		a.values = append(a.values, e)
+		return nil
+	}
+
+	a.values = append(a.values[:i+1], a.values[i:]...)
+	a.values[i] = e
+	return nil
+}
+
+func(a *ArrayList[T]) AddAllAt(i int, es []T) error {
+	if i < 0 || len(a.values) < i {
+		return InvalidIndexErr
+	}
+
+	a.values = append(a.values[:i], append(es, a.values[i:]...)...)
+	return nil
 }
--- arraylist/arraylist_test.go
+++ arraylist/arraylist_test.go
@@ -5,21 +5,63 @@ import (
 	"testing"
 )
 
-func TestAppend(t *testing.T) {
+func TestAdd(t *testing.T) {
 	al := New[int]()
-	al.Append(1)
-	al.Append(2)
+	al.Add(1)
+	al.Add(2)
 	if !reflect.DeepEqual(al.values, []int{1, 2}) {
 		t.Errorf("expected: %v but got %v", []int{1, 2}, al.values)
 	}
 }
 
-func TestAppendAll(t *testing.T) {
+func TestAddAll(t *testing.T) {
 	al := New[int]()
-	al.Append(1)
-	al.AppendAll([]int{2, 3, 4})
-	al.AppendAll([]int{5})
+	al.Add(1)
+	al.AddAll([]int{2, 3, 4})
+	al.AddAll([]int{5})
 	if !reflect.DeepEqual(al.values, []int{1, 2, 3, 4, 5}) {
 		t.Errorf("expected: %v but got %v", []int{1, 2, 3, 4, 5}, al.values)
+	}
+}
+
+func TestAddAt(t *testing.T) {
+	al := New[int]()
+	if err := al.AddAt(0, 1); err != nil {
+		t.Errorf("unexpected err: %s", err)
+	}
+
+	if err := al.AddAt(3, 2); err == nil {
+		t.Errorf("err should be returned")
+	}
+
+	if err := al.AddAt(2, 2); err == nil {
+		t.Errorf("err should be returned")
+	}
+
+	if err := al.AddAt(1, 2); err != nil {
+		t.Errorf("unexpected err: %s", err)
+	}
+
+	if err := al.AddAt(1, 3); err != nil {
+		t.Errorf("unexpected err: %s", err)
+	}
+
+	if err := al.AddAt(0, 4); err != nil {
+		t.Errorf("unexpected err: %s", err)
+	}
+
+	if !reflect.DeepEqual(al.values, []int{4, 1, 3, 2}) {
+		t.Errorf("expected: %v but got %v", []int{4, 1, 3, 2}, al.values)
+	}
+}
+
+func TestAddAllAt(t *testing.T) {
+	al := New[int]()
+	if err := al.AddAllAt(0, []int{1}); err != nil {
+		t.Errorf("unexpected err: %s", err)
+	}
+
+	if !reflect.DeepEqual(al.values, []int{1}) {
+		t.Errorf("expected: %v but got %v", []int{1}, al.values)
 	}
 }
