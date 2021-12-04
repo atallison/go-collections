b39b3be956b3844877c0577b0dcbfdd04039ba49
Hidetatz Yaginuma <hidetatz@gmail.com>
2021-12-04 18:13:08 +0900

    add comments

--- arraylist.go
+++ arraylist.go
@@ -1,21 +1,30 @@
 package collection
 
+// ArrayList is a variable-sized list.
+// This data structure is not concurrent safe.
+// The caller must be responsible to synchronize before accessing from multiple goroutines.
 type ArrayList[T any] struct {
 	values []T
 }
 
+// NewArrayList returns an ArrayList based on the specified type.
 func NewArrayList[T any]() *ArrayList[T] {
 	return &ArrayList[T]{values: []T{}}
 }
 
+// Add appends a given value to the bottom of the list.
 func (a *ArrayList[T]) Add(e T) {
 	a.values = append(a.values, e)
 }
 
+// AddAll appends given values to the bottom of the list in the given order.
 func (a *ArrayList[T]) AddAll(es []T) {
 	a.values = append(a.values, es...)
 }
 
+// AddAt inserts the given element at the given index.
+// When the given is less than 0 or greater than the list size,
+// ErrInvalidIndex will be responded.
 func (a *ArrayList[T]) AddAt(i int, e T) error {
 	if i < 0 || len(a.values) < i {
 		return ErrInvalidIndex
@@ -31,6 +40,11 @@ func (a *ArrayList[T]) AddAt(i int, e T) error {
 	return nil
 }
 
+// AddAllAt inserts the given values at the given index.
+// Let's say current list looks [a, b, c] then
+// calls AddAllAt(1, [d, e]), it results [a, d, e, b, c].
+// When the given index is less than 0 or greater than the list size,
+// ErrInvalidIndex will be responded.
 func (a *ArrayList[T]) AddAllAt(i int, es []T) error {
 	if i < 0 || len(a.values) < i {
 		return ErrInvalidIndex
--- error.go
+++ error.go
@@ -3,5 +3,6 @@ package collection
 import "errors"
 
 var (
+	// ErrInvalidIndex indicates the given index is invalid for the operation.
 	ErrInvalidIndex = errors.New("invalid index")
 )
