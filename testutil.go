ee953b46efa2b9f5c759dab0910c1515fd8b8c07
Hidetatz Yaginuma <hidetatz@gmail.com>
2021-12-04 10:14:40 +0900

    add utility for test

---
+++ testutil.go
@@ -0,0 +1,35 @@
+package collection
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+)
+
+func MustEqual(t *testing.T, expected, got interface{}) {
+	t.Helper()
+	if !reflect.DeepEqual(expected, got) {
+		t.Errorf("expected: %v but got %v", expected, got)
+	}
+}
+
+func MustBeNil(t *testing.T, v interface{}) {
+	t.Helper()
+	if v != nil {
+		t.Errorf("nil is expected but got %v", v)
+	}
+}
+
+func MustBeNonNil(t *testing.T, v interface{}) {
+	t.Helper()
+	if v == nil {
+		t.Errorf("non-nil is expected but got nil")
+	}
+}
+
+func MustBeErr(t *testing.T, expected, got error) {
+	t.Helper()
+	if !errors.Is(got, expected) {
+		t.Errorf("expected error: %v but got %v", expected, got)
+	}
+}
