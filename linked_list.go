3d0a020fc1e0a023e226749002add85448b710cc
Hidetatz Yaginuma <hidetatz@gmail.com>
2021-12-21 22:37:00 +0900

    add some generic funcs

--- linked_list.go
+++ linked_list.go
@@ -3,6 +3,7 @@ package collection
 // LinkedList is an implementation of doubly linked list.
 // This is not concurrent safe.
 type LinkedList[T any] struct {
+	// dummy.next is head, dummy.prev is tail.
 	dummy  *singlyLinkedNode[T]
 	length int
 }
@@ -11,4 +12,30 @@ type linkedNode[T any] struct {
 	v    T
 	next *linkedNode[T]
 	prev *linkedNode[T]
+}
+
+func (n *linkedNode[T]) next() *linkedNode[T] {
+	return n.next
+}
+
+func (n *linkedNode[T]) previous() *linkedNode[T] {
+	return n.prev
+}
+
+func NewLinkedList[T any]() *LinkedList[T] {
+	var zero T
+	dummy := &linkedNode{v: zero}
+	dummy.next, dummy.prev = dummy, dummy
+	return &LinkedList[T]{
+		dummy:  dummy,
+		length: 0,
+	}
+}
+
+func (l *LinkedList[T]) Head() T {
+	return l.dummy.next.v
+}
+
+func (l *LinkedList[T]) Tail() T {
+	return l.dummy.prev.v
 }
