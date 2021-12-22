7ad2a30f67a3d79faf802dd5db6f36b961ccea7d
Hidetatz Yaginuma <hidetatz@gmail.com>
2021-12-22 23:49:33 +0900

    define some common methods

--- linked_list.go
+++ linked_list.go
@@ -8,16 +8,39 @@ type LinkedList[T any] struct {
 	length int
 }
 
+// linkedNode is a node in LinkedList.
 type linkedNode[T any] struct {
-	v    T
-	next *linkedNode[T]
-	prev *linkedNode[T]
+	v        T
+	prevNode *linkedNode[T]
+	nextNode *linkedNode[T]
 }
 
+func (n *linkedNode[T]) previous() *linkedNode[T] {
+	return n.prevNode
+}
+
+func (n *linkedNode[T]) next() *linkedNode[T] {
+	return n.nextNode
+}
+
+// insertAfter inserts given valud to the next to n.
+// Newly added node will be returned.
+func (n *linkedNode[T]) insertAfter(v T) *linkedNode[T] {
+	m := &linkedNode[T]{
+		v:        v,
+		prevNode: n,
+		nextNode: n.nextNode,
+	}
+	n.nextNode.prevNode = m
+	n.nextNode = m
+	return m
+}
+
+// NewLinkedList returns an empty LinkedList based on the specified type T.
 func NewLinkedList[T any]() *LinkedList[T] {
-	var zero T
+	var zero T // zero value
 	dummy := &linkedNode[T]{v: zero}
-	dummy.next, dummy.prev = dummy, dummy
+	dummy.nextNode, dummy.prevNode = dummy, dummy
 	return &LinkedList[T]{
 		dummy:  dummy,
 		length: 0,
@@ -25,9 +48,9 @@ func NewLinkedList[T any]() *LinkedList[T] {
 }
 
 func (l *LinkedList[T]) Head() T {
-	return l.dummy.next.v
+	return l.dummy.next().v
 }
 
 func (l *LinkedList[T]) Tail() T {
-	return l.dummy.prev.v
+	return l.dummy.previous().v
 }
